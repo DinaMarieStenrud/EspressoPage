c35de10594ee6deca0e20179c7eaebb8fc4a5dbe
Dina Marie Stenrud <dinamst@ntnu.no>
2025-10-01 10:46:01 +0200

    Build med riktig sti til recipe.ts

---
+++ docs/assets/recipe-DP8K_f6a.js
@@ -0,0 +1,26 @@
+import"./style-D_2xh1s-.js";document.querySelector("#app").innerHTML=`
+
+
+  <div>
+    <h1> Espresso recipe </h1>
+
+    <ul>
+        <li>Espresso beans</li>
+        <li>Espresso machine</li>
+
+    </ul>
+
+    <div>
+        <img id="machine" src="images/machine.png"/>
+
+        <div class="beans">
+
+
+        </div>
+  </div>
+
+
+
+
+
+`;
